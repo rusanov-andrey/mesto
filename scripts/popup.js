6918c5d218776cc3638298e6e44bbbaf3cd9f403
Rusanov Andrey <rusanov.newmail@yandex.ru>
2023-04-17 22:13:21 +0300

    Добавлено закрытие по клику на оверлее

---
+++ scripts/popup.js
@@ -0,0 +1,30 @@
+function addPopupCloseFunction() {
+  const popupArray = Array.from(document.querySelectorAll('.popup'));
+
+  popupArray.forEach(popup => {
+    popup.addEventListener('click', (evt) => {
+      closePopup();
+    })
+  })
+}
+
+function catchClick(element)
+{
+  element.addEventListener('click', (evt) => {
+    evt.stopPropagation();
+  })
+}
+
+function configureCloseFunction()
+{
+  addPopupCloseFunction()
+
+  const image = document.querySelector('.popup__photo-image');
+  catchClick(image)
+
+  const formArray = Array.from(document.querySelectorAll('.popup__form'));
+  formArray.forEach(form => catchClick(form));
+
+}
+
+configureCloseFunction();
