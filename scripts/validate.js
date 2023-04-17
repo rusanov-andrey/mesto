7e3dcc7d254417e60a39f9413c91dfef916b0dbb
Rusanov Andrey <rusanov.newmail@yandex.ru>
2023-04-17 10:32:51 +0300

    Добавлена обработка ошибок в JS

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,12 +1,23 @@
-function showInputError(input, errorClass) {
-  const error_id = "${input.id}-error";
+function showInputError(input, errorMessageClass) {
+  const error_id = `#${input.id}-error`;
+  console.log(`error-id: ${error_id}`);
   const errorElement = document.querySelector(error_id);
-  errorElement.classList.add(errorClass);
+  errorElement.classList.add(errorMessageClass);
+  errorElement.textContent = input.validationMessage;
 }
 function hideInputError(input, errorClass) {
-  const error_id = "${input.id}-error";
+  const error_id = `#${input.id}-error`;
   const errorElement = document.querySelector(error_id);
-  errorElement.classList.add(errorClass);
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = ' ';
+}
+
+function updateButtonState(button, inputArray) {
+  const disabled = inputArray.some(x => !x.validity.valid);
+  if(disabled)
+    button.disabled = true;
+  else
+    button.disabled = false;
 }
 
 
@@ -16,17 +27,30 @@ function enableFormValidation(options) {
   const submitButton = currentForm.querySelector('.popup__submit');
 
   inputArray.forEach(input => {
+    input.addEventListener('input', function (evt) {
+      if(input.validity.valid)
+      {
+        hideInputError(input, options.errorMessageClass);
+        return;
+      }
 
+      showInputError(input, options.errorMessageClass);
+    })
+  })
+
+  currentForm.addEventListener('input', function (evt) {
+    updateButtonState(submitButton, inputArray);
   })
 }
 
 function enableValidation(options) {
-  const formArray = Array.from(document.querySelectorAll('.form'));
+  const formArray = Array.from(document.querySelectorAll('.popup__form'));
 
   formArray.forEach(form => {
     const opt = Object.assign( {}, options);
     opt.form = form;
     enableFormValidation(opt);
+    console.log(opt);
   })
 }
 
@@ -35,5 +59,5 @@ enableValidation({
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__submit',
   inactiveButtonClass: 'popup__submit_disabled',
-  errorClass: 'popup__error_visible'
-})
+  errorMessageClass: 'popup__error_visible'
+});
