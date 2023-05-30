6ca12c907f29a5b44a85a67fc99fd9d2e6603ea2
Rusanov Andrey <rusanov.newmail@yandex.ru>
2023-05-30 20:31:20 +0300

    Поправил валидацию

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,13 @@ import { initialCards } from '../utils/initial-cards.js';
 import { CardGalary } from '../scripts/CardGalary';
 import {UserInfoEditor} from '../scripts/UserInfoEditor.js';
 import { ValidatorFactory } from '../scripts/ValidatorFactory';
+import { validatorOptions } from '../utils/constants.js';
 
 const galary = new CardGalary(initialCards);
 galary.render();
 
 const userInfo = new UserInfoEditor({nameSelector: '.profile__title', aboutSelector: '.profile__subtitle' });
 
-const factory = new ValidatorFactory({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit',
-  inactiveButtonClass: 'popup__submit_disabled',
-  errorMessageClass: 'popup__error_visible'
-});
+const factory = new ValidatorFactory(validatorOptions);
 
 factory.createValidators();
